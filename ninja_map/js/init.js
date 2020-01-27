da171252e9bcbed10f82e1abf1fabcebf510fa6e
JamesKinler <james193722@yahoo.com>
2020-01-27 11:02:28 -0800

    Update init.js
    Changed the code to listen to the form on render instead of load. Because on load the form was not loading when you hit refresh. Because ninja forms is async and the form doesn't load again.

--- ninja_map/js/init.js
+++ ninja_map/js/init.js
@@ -1,21 +1,29 @@
 // Create a new object for custom validation of a custom field.
-var mySubmitController = Marionette.Object.extend( {
+var myCustomFieldController = Marionette.Object.extend( {
 
-  initialize: function() {
-    this.listenTo( Backbone.Radio.channel( 'forms' ), 'forms:loaded');
-    var footer= document.getElementsByTagName('footer')[0];
-     var script= document.createElement('script');
-     script.type= 'text/javascript';
-     script.src= "https://maps.googleapis.com/maps/api/js?key=YOURAPIKEYGOESHERE&libraries=places&callback=initMap";
-     footer.appendChild(script);
-},
- });
+	initialize: function() {
 
+		// Listen to the render:view event for a field type. Example: Textbox field.
+		this.listenTo( nfRadio.channel( 'form' ), 'render:view', this.renderView );
+	},
 
+	renderView: function( view ) {
+		// Check if this is the right field. Example check for field key.
+		if ( 'contact-map' != view.model.get( 'key' ) ) return false;
+    // then its finding the class on the element
+		var el = jQuery( view.el ).find( '.contact-form-map' );
+    console.log('google maps form is loaded');
+    // then appending the script to the body
+        var body= document.getElementsByTagName('body')[0];
+         var script= document.createElement('script');
+         script.type= 'text/javascript';
+         script.src= "https://maps.googleapis.com/maps/api/js?key=YOURAPIKEYHERE8&libraries=places&callback=initMap";
+         document.body.appendChild(script);
+	}
+});
 
 // On Document Ready...
 jQuery( document ).ready( function( $ ) {
-    console.log('Loaded Jquery');
-    // Instantiate our custom field's controller, defined above.
-    new mySubmitController();
+	// Instantiate our custom field's controller, defined above.
+	new myCustomFieldController();
 });
